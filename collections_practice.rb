140bc6f1eaaeb28a5f84a1f62b0201cdbb18fdc7
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-13 15:42:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,7 @@ end
 def kesha_maker(array)
  new_array = []
  array.each do |word|
-    new_array << word.scan(/.{3}|.+/).join("$")
+    new_array << word.scan(/.{3}|.+/) = "$"
   end
 new_array
 end
