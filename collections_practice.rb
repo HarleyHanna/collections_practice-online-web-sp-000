e738e3e29901d0f9df23d6aa1395c27fc706f801
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-13 16:05:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ def kesha_maker(array)
  new_string = ""
  new_array = []
  array.each do |word|
-    new_string = word.sub(/.{2}|.+/, "$")
+    new_string = word.sub(/(?:\S{3}?)/, "$")
     new_array << new_string
   end
 new_array
